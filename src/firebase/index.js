c264a49b7ad0637d195400bb8903dc1ec48c05ec
jeehwan <tjtojan@naver.com>
2022-07-28 02:17:14 +0900

    로그인 기능 구현1

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU2ncAIrL6kmB85nmppBUIP4otE6hV6s8",
+  authDomain: "slack-clone-e8d74.firebaseapp.com",
+  databaseURL: "https://slack-clone-e8d74-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "slack-clone-e8d74",
+  storageBucket: "slack-clone-e8d74.appspot.com",
+  messagingSenderId: "632534741193",
+  appId: "1:632534741193:web:0e654171f6889628ed9d18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore } from 'redux';
+import rootReducer from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={legacy_createStore(rootReducer)}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
